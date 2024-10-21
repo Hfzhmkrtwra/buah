8a59924efff5f1c799cb1a0c334e51e4d3bcd817
Hfzhmkrtwra <hafizahkartawiria701@gmail.com>
2024-10-21 10:46:28 +0700

    Menambah fungsi tambah data buah

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
-import { log } from './logger'
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js'
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDocs,
+  getDoc,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  query,
+  orderBy
+} from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDGYnq4VKq-YGu4RbfoI_ZHez9fishYjZo",
+  authDomain: "insan-cemerlang-afd2f.firebaseapp.com",
+  projectId: "insan-cemerlang-afd2f",
+  storageBucket: "insan-cemerlang-afd2f.appspot.com",
+  messagingSenderId: "686649580589",
+  appId: "1:686649580589:web:61374bbbd68adb604eaca4",
+  measurementId: "G-LNZTQBCE26"
+};
 
-log('Import works!')
+//inisialisasi firebase
+const aplikasi = initializeApp(firebaseConfig)
+const basisdata = getFirestore(aplikasi)
+
+export async function tambahBuah(nama, warna, harga) {
+  try {
+    // menyimpan data ke firebase
+    const refDokumen = await addDoc(collection(basisdata, "buah"), {
+      nama: nama,
+      warna: warna,
+      harga: harga
+    })
+
+    // menampilkan pesan berhasil
+    console.log('berhasil menyimpan data buah')
+  } catch (error) {
+    // menampilkan pesan gagal 
+    console.log('gagal menyimpan data buah' + error)
+  }
+}
